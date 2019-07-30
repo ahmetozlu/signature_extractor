ca9b456df7bde9fbd55ab9fe6f882672da848dbf
maky-hnou <haniyousfi@gmail.com>
2019-07-30 23:31:58 +0100

    style: formatting the code and adding docstrings

--- sample_project/main.py
+++ sample_project/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+"""Python main file."""
 # -*- coding: utf-8 -*-
 # -----------------------------------------
 # author      : Ahmet Ozlu
@@ -6,45 +7,51 @@
 # date        : 05.05.2019
 # -----------------------------------------
 
+import color_correlation
 import cv2
 import dewapper
-import unsharpen
-import color_correlation
 import signature_extractor
+import unsharpen
 
 source_image = cv2.imread("test.jpg")
 img = 0
-try:	
-	# read the source input image and call the dewarp_book function to perform cropping with the margin and book dewarping
-	img = dewapper.dewarp_book(source_image)
-	cv2.imwrite("step 1 - page_dewarped.jpg", img)
-	print("- step1 (cropping with the argins + book dewarpping): OK")
-except Exception as e:
-	print("type error: " + str(e))
-	print("ERROR IN CROPPING & BOOK DEWARPING! PLEASE CHECK LIGTHNING, SHADOW, ZOOM LEVEL AND ETC. OF YOUR INPUT BOOK IMAGE!")
 try:
-	# call the unsharpen_mask method to perform signature extraction
-	img = signature_extractor.extract_signature(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
-	cv2.imwrite("step 2 - signature_extracted.jpg", img)
-	print("- step2 (signature extractor): OK")
+    # read the source input image and call the dewarp_book function
+    # to perform cropping with the margin and book dewarping
+    img = dewapper.dewarp_book(source_image)
+    cv2.imwrite("step 1 - page_dewarped.jpg", img)
+    print("- step1 (cropping with the argins + book dewarpping): OK")
 except Exception as e:
-	print("type error: " + str(e))
-	print("ERROR IN SIGNATURE EXTRACTION! PLEASE CHECK LIGTHNING, SHADOW, ZOOM LEVEL AND ETC. OF YOUR INPUT BOOK IMAGE!")
+    print("type error: " + str(e))
+    print("ERROR IN CROPPING & BOOK DEWARPING! PLEASE CHECK LIGTHNING,"
+          " SHADOW, ZOOM LEVEL AND ETC. OF YOUR INPUT BOOK IMAGE!")
 try:
-	# call the unsharpen_mask method to perform unsharpening mask
-	unsharpen.unsharpen_mask(img)
-	cv2.imwrite("step 3 - unsharpen_mask.jpg", img)
-	print("- step3 (unsharpening mask): OK")
+    # call the unsharpen_mask method to perform signature extraction
+    img = signature_extractor.extract_signature(cv2.cvtColor(img,
+                                                cv2.COLOR_BGR2GRAY))
+    cv2.imwrite("step 2 - signature_extracted.jpg", img)
+    print("- step2 (signature extractor): OK")
 except Exception as e:
-	print("type error: " + str(e))
-	print("ERROR IN BOOK UNSHARPING MASK! PLEASE CHECK LIGTHNING, SHADOW, ZOOM LEVEL AND ETC. OF YOUR INPUT BOOK IMAGE!")
+    print("type error: " + str(e))
+    print("ERROR IN SIGNATURE EXTRACTION! PLEASE CHECK LIGTHNING, SHADOW,"
+          " ZOOM LEVEL AND ETC. OF YOUR INPUT BOOK IMAGE!")
 try:
-	# call the funcBrightContrast method to perform color correction
-	img = color_correlation.funcBrightContrast(img)
-	cv2.imwrite("step 4 - color_correlated.jpg", img)
-	print("- step4 (color correlation): OK")
+    # call the unsharpen_mask method to perform unsharpening mask
+    unsharpen.unsharpen_mask(img)
+    cv2.imwrite("step 3 - unsharpen_mask.jpg", img)
+    print("- step3 (unsharpening mask): OK")
 except Exception as e:
-	print("type error: " + str(e))
-	print("ERROR IN BOOK COLOR CORRELATION! PLEASE CHECK LIGTHNING, SHADOW, ZOOM LEVEL AND ETC. OF YOUR INPUT BOOK IMAGE!")
+    print("type error: " + str(e))
+    print("ERROR IN BOOK UNSHARPING MASK! PLEASE CHECK LIGTHNING, SHADOW,"
+          " ZOOM LEVEL AND ETC. OF YOUR INPUT BOOK IMAGE!")
+try:
+    # call the funcBrightContrast method to perform color correction
+    img = color_correlation.funcBrightContrast(img)
+    cv2.imwrite("step 4 - color_correlated.jpg", img)
+    print("- step4 (color correlation): OK")
+except Exception as e:
+    print("type error: " + str(e))
+    print("ERROR IN BOOK COLOR CORRELATION! PLEASE CHECK LIGTHNING, SHADOW,"
+          " ZOOM LEVEL AND ETC. OF YOUR INPUT BOOK IMAGE!")
 
 cv2.imwrite("output.jpg", img)
--- signature_extractor.py
+++ signature_extractor.py
@@ -1,18 +1,15 @@
-#----------------------------------------------
-#--- Author         : Ahmet Ozlu
-#--- Mail           : ahmetozlu93@gmail.com
-#--- Date           : 17th September 2018
-#----------------------------------------------
+"""Extract signatures from an image."""
+# ----------------------------------------------
+# --- Author         : Ahmet Ozlu
+# --- Mail           : ahmetozlu93@gmail.com
+# --- Date           : 17th September 2018
+# ----------------------------------------------
 
 import cv2
-import numpy as np
-from skimage import measure
-from skimage.measure import label, regionprops
-from skimage.color import label2rgb
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-from scipy import ndimage
-from skimage import morphology
+from skimage import measure, morphology
+from skimage.color import label2rgb
+from skimage.measure import regionprops
 
 # read the input image
 img = cv2.imread('./inputs/sample_scanned_document_#1.jpg', 0)
@@ -38,32 +35,34 @@ total_area = 0
 counter = 0
 average = 0.0
 for region in regionprops(blobs_labels):
-    if region.area>10:
+    if (region.area > 10):
         total_area = total_area + region.area
         counter = counter + 1
-    #print region.area # (for debugging)
+    # print region.area # (for debugging)
     # take regions with large enough areas
-    if region.area >= 250:
+    if (region.area >= 250):
         if (region.area > the_biggest_component):
             the_biggest_component = region.area
 
 average = (total_area/counter)
-print ("the_biggest_component: " + str(the_biggest_component))
-print ("average: " + str(average))
+print("the_biggest_component: " + str(the_biggest_component))
+print("average: " + str(average))
 
-# experimental-based ratio calculation, modify it for your cases 
-# a4_constant is used as a threshold value to remove connected pixels are smaller than a4_constant for A4 size scanned documents
+# experimental-based ratio calculation, modify it for your cases
+# a4_constant is used as a threshold value to remove connected pixels
+# are smaller than a4_constant for A4 size scanned documents
 a4_constant = ((average/84.0)*250.0)+100
-print ("a4_constant: " + str(a4_constant))
+print("a4_constant: " + str(a4_constant))
 
 # remove the connected pixels are smaller than a4_constant
 b = morphology.remove_small_objects(blobs_labels, a4_constant)
-# save the the pre-version which is the image is labelled with colors as considering connected components
+# save the the pre-version which is the image is labelled with colors
+# as considering connected components
 plt.imsave('pre_version.png', b)
 
 # read the pre-version
 img = cv2.imread('pre_version.png', 0)
 # ensure binary
-img = cv2.threshold(img, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 # save the the result
 cv2.imwrite("./outputs/output.png", img)
